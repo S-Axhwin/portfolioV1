44626e98b94faa7f736e371c093e7b14acae34dd
S-Axhwin <ashwin.sathiyamoorthy.d@gmail.com>
2024-04-08 11:23:46 +0530

    changed routers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { Route, Routes,  BrowserRouter } from "react-router-dom";
+import { Route, Routes,  HashRouter } from "react-router-dom";
 import Home from './components/Home'
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
   return (
-    <BrowserRouter basename="/portfolioV1">
+    <HashRouter>
     <Routes>
       <Route path="/" element={<Home/>} ></Route>
       <Route path="/projects" element={<Projects/>} ></Route>
       <Route path="/contact" element={<Contact/>} ></Route>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
